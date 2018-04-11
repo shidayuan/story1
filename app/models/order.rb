d1c58bc9191401b2218cf94aa78013f682124989
shichengyuan <15036062755@163.com>
2018-04-11 09:54:59 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+
+  belongs_to :user
+
+  validates :billing_name, present: true
+  validates :billing_address, present:true
+  validates :shipping_name, present:true
+  validates :shipping_address, present:true 
 end
